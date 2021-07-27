40c2a7bbf9a47c53339ffc04b86b4d17c62bdcc5
Tyler Holdren <tylerholdren@Tylers-MacBook-Pro.local>
2021-07-27 19:47:29 -0400

    functionality to getResults

--- client/Routes/SearchPage.tsx
+++ client/Routes/SearchPage.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import SearchResult from '../Components/SearchResult';
 
-import SearchBar from '../Components/SearchBar';
+// import SearchBar from '../Components/SearchBar';
 import Filters from '../Components/Filters';
 import AllSearchResults from '../Components/AllSearchResults';
 
 const SearchPage: FC<any> = (props: any) => {
+  const [results, setResults] = useState<any>([]);
+
+  // results.forEach(object => {
+  //   <SearchResult
+  //     // key={i++}
+  //     companyName={object.companyName}
+  //     title={object.title}
+  //     description={object.description}
+  //     location={object.location.name}
+  //     link={object.link}
+  //   />;
+  // });
+
   return (
     <div>
-      <SearchBar />
-      <Filters />
-      <AllSearchResults />
+      <Filters setResults={setResults} />
+      <AllSearchResults state={results} />
     </div>
   );
 };
