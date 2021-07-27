180e7efc13da923b54f4c7edc46cfcf0497be1e8
Tyler Holdren <tylerholdren@Tylers-MacBook-Pro.local>
2021-07-26 20:22:42 -0400

    SearchBar Logic

--- client/Components/AllSearchResults.tsx
+++ client/Components/AllSearchResults.tsx
@@ -1,17 +1,16 @@
-// import React, {FC} from "react"; 
-// import { render } from "react-dom";
+import React, { FC } from 'react';
+import { render } from 'react-dom';
 
-// import SearchResult from "./SearchResult"; 
+import SearchResult from './SearchResult';
 
-// const SearchPage: FC<any> = (props: any) => {
+const AllSearchResults: FC<any> = (props: any) => {
+  return (
+    <div>
+      {/* {this.props.state.map({CompanyName, Description, Position, ExperienceLevel} => {
+        return <SearchResult key={[i]} CompanyName={CompanyName} Description={Description} Position={Position} ExperienceLevel={ExperienceLevel}/>;
+      })} */}
+    </div>
+  );
+};
 
-// //create a new search result component for each result we get back from the api, loop through response object
-
-//   return (
-//     <div>
-//       <SearchResult />
-//     </div>
-//   )
-// }
-
-// export default SearchPage; 
+export default AllSearchResults;
--- client/Components/SearchResult.tsx
+++ client/Components/SearchResult.tsx
@@ -1,18 +1,19 @@
-import React, {FC} from "react"; 
-import { render } from "react-dom"; 
+import React, { FC } from 'react';
+import { render } from 'react-dom';
 
 const SearchResult: FC<any> = (props: any) => {
-
-  {/* <p className="Salary-Range"></p> */}
+  {
+    /* <p className="Salary-Range"></p> */
+  }
 
   return (
     <div className="SearchResult">
-      <p className="Company-Name"></p>
+      <p className="CompanyName"></p>
       <p className="Description"></p>
       <p className="Position"></p>
-      <p className="Experience-level"></p>
+      <p className="ExperienceLevel"></p>
     </div>
-  )
-}
+  );
+};
 
-export default SearchResult; 
+export default SearchResult;
