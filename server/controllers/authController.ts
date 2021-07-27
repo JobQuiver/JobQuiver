9db0a4e349156ab5d2472a9041c2d6ecd7a2b0a5
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-07-27 19:51:27 -0400

    implement JWT

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
+import jsonwebtoken from 'jsonwebtoken';
 
-const authController = {
+const SECRETKEY = 'LOLLLLLLLSSSSSSS';
+
+export const authController = {
   verifyUser: async (req: Request, res: Response, next: NextFunction)=> {
     const {username, password} = req.body;
     if(username && password){
@@ -9,8 +12,46 @@ const authController = {
     next();
   },
 
-  createCookie: (req: Request, res: Response, next: NextFunction) =>  {
-    
+  createCookie: async (req: Request, res: Response, next: NextFunction) =>  {
+    let tokenKey;
+    await jsonwebtoken.sign({ foo: 'bar' }, SECRETKEY, function(err, token){
+      console.log(token);
+      tokenKey = token
+    });
+    console.log(tokenKey);
+    res.cookie('tokenKey', tokenKey);
+    await jsonwebtoken.verify(tokenKey, SECRETKEY, function(err, decoded) {
+      if (err) {
+        /*
+          err = {
+            name: 'TokenExpiredError',
+            message: 'jwt expired',
+            expiredAt: 1408621000
+          }
+        */
+      }
+      else{
+        console.log(decoded);
+      }
+    });
+    next();
+  },
+  verify: async (req: Request, res: Response, next: NextFunction) => {
+    // await jsonwebtoken.verify(tokenKey, 'shhhhh', function(err, decoded) {
+    //   if (err) {
+    //     /*
+    //       err = {
+    //         name: 'TokenExpiredError',
+    //         message: 'jwt expired',
+    //         expiredAt: 1408621000
+    //       }
+    //     */
+    //   }
+    //   else{
+    //     console.log(decoded);
+    //   }
+    // });
+    next();
   }
   
 };
--- server/routes/login.ts
+++ server/routes/login.ts
@@ -1,14 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express';
-
+import { authController } from '../controllers/authController';
 
 const loginRouter = Router();
 
-loginRouter.use('/auth', (req: Request, res: Response) => {
-  res.status(200);
+loginRouter.post('/auth', authController.createCookie, (req: Request, res: Response) => {
+  res.status(200).send();
 })
 
-loginRouter.use('/oauth/google', (req: Request, res: Response) => {
-  res.status(200);
+loginRouter.post('/auth/verify', authController.verify, (req: Request, res: Response) => {
+  res.status(200).send();
+})
+
+loginRouter.post('/oauth/google', (req: Request, res: Response) => {
+  res.status(200).send();
 })
 
 export default loginRouter;
