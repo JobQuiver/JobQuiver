73d54dc251a3d22d52e49b1f57c218a9ace5bc65
David Zhang <davidzhang@Davids-MacBook-Air.local>
2021-07-28 19:20:20 -0400

    complete collapse feature and css fix

--- client/Components/SearchResult.tsx
+++ client/Components/SearchResult.tsx
@@ -1,27 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import Description from './Description';
 import SaveButton from './SaveButton';
 
-const SearchResult: FC<any> = (props: any) => (
-  <div>
-    <div className="SearchResult">
-      <div className="TitleAndSave">
-        <p className="companyName">{props.companyName}</p>
-        <button className="ExpandButton">expand +</button>
-        <SaveButton {...props} />
+const SearchResult: FC<any> = (props: any) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const handleClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const inlineStyling = isCollapsed ? null : { maxHeight: 'none', overflow: 'auto' };
+
+  return (
+    <div>
+      <div className="SearchResult">
+        <div className="TitleAndSave">
+          <p className="companyName">{props.companyName}</p>
+          <button
+            className="ExpandButton"
+            onClick={handleClick}
+          >{isCollapsed ? 'expand +' : 'hide -'}</button>
+          <SaveButton {...props} />
+        </div>
+        <p className="title">{props.title}</p>
+        <div className="description-container" style={inlineStyling}>
+          <Description description={props.description} />
+        </div>
+        <p className="location">{props.location}</p>
+        <p>
+          {'See original posting '}
+          <a href={props.link} className="link">
+            here
+          </a>
+        </p>
       </div>
-      <p className="title">{props.title}</p>
-      <Description description={props.description} />
-      <p className="location">{props.location}</p>
-      <p>
-        {'See original posting '}
-        <a href={props.link} className="link">
-          here
-        </a>
-      </p>
     </div>
-  </div>
-);
+  )
+}
 
 export default SearchResult;
