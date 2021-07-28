8344a2160c323533b53566b688e282c660d42c33
David Anderson <dlande000@gmail.com>
2021-07-28 11:08:29 -0400

    Update FavoritesPage

--- client/Components/PreviousSearches.tsx
+++ client/Components/PreviousSearches.tsx
@@ -1,29 +1,29 @@
-// import React, { FC } from 'react';
-// import PreviousSearch from './PreviousSearch';
+import React, { FC } from 'react';
+import PreviousSearch from './PreviousSearch';
 
-// const PreviousSearches: FC<any> = (props: any) => {
-//   return (
-//     <div>
-//       {props.state.map(
-//         (
-//           { SavedName, CompanyName, Description, Position, ExperienceLevel },
-//           i
-//         ) => {
-//           return (
-//             <PreviousSearch
-//               key={[i]}
-//               //Do we want the user to have the optionality of naming the search?
-//               SavedName={SavedName}
-//               CompanyName={CompanyName}
-//               Description={Description}
-//               Position={Position}
-//               ExperienceLevel={ExperienceLevel}
-//             />
-//           );
-//         }
-//       )}
-//     </div>
-//   );
-// };
+const PreviousSearches: FC<any> = (props: any) => {
+  return (
+    <div>
+      {props.state.map(
+        (
+          { SavedName, CompanyName, Description, Position, ExperienceLevel },
+          i
+        ) => {
+          return (
+            <PreviousSearch
+              key={[i]}
+              //Do we want the user to have the optionality of naming the search?
+              SavedName={SavedName}
+              CompanyName={CompanyName}
+              Description={Description}
+              Position={Position}
+              ExperienceLevel={ExperienceLevel}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+};
 
-// export default PreviousSearches;
+export default PreviousSearches;
--- client/Routes/FavoritesPage.tsx
+++ client/Routes/FavoritesPage.tsx
@@ -1,11 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import PreviousSearches from '../Components/PreviousSearches';
 
+const MY_SAVED_RESULTS = 'My Saved Results';
+const SAVED_RESULTS_ROUTE = '/savedResults';
+const ERROR_TEXT = 'An error has occurred. Please refresh and try again.'
+interface SavedResults {
+  title: string,
+  location: string,
+  description: string,
+  link: string,
+  companyName: string,
+  apiWebsite: string,
+  apiId: number,
+};
+
 const FavoritesPage: FC<any> = (props: any) => {
+  const [savedResults, setSavedResults] = useState<SavedResults[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(SAVED_RESULTS_ROUTE)
+      .then(res => res.json())
+      .then(newSavedResults => setSavedResults(newSavedResults))
+      .catch(err => {
+        console.log(`An error has occurred in the FavoritesPage component while getting saved results: ${err}`);
+        setError(ERROR_TEXT);
+      });
+  }, []);
+
   return (
     <div>
-      <p>My Recent Searches</p>
-      <PreviousSearches />
+      <p>{MY_SAVED_RESULTS}</p>
+      {error && <div>{error}</div>}
+      <PreviousSearches savedResults={savedResults}/>
     </div>
   );
 };
