724aced2bee5aedb02db9815b97b9e958861112a
Tyler Holdren <tylerholdren@Tylers-MacBook-Pro.local>
2021-07-28 14:54:29 -0400

    css updates + expand functionality

---
+++ client/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Header: FC<any> = (props: any) => {
+  return (
+    <div className="Header HeaderShadow">
+      <p>JobQuiver</p>
+    </div>
+  );
+};
+
+export default Header;
